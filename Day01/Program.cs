072bf80ab5dd9093194022ce50456d1e1a2d3983
Johan Busk Eriksson <johan.busk.eriksson@liu.se>
2024-12-02 14:07:38 +0100

    Add project files.

---
+++ Day01/Program.cs
@@ -0,0 +1,7 @@
+﻿var lines = File.ReadAllLines("input.txt").Select(x => x.Split("   ")).Select(x => (int.Parse(x[0]), int.Parse(x[1])));
+var group = lines.Select(x => x.Item2).GroupBy(i => i)
+    .Select(g => new KeyValuePair<int, int>(g.Key, g.Count())).ToDictionary();
+var sumpart1 = lines.Select(x => x.Item1).Order().Zip(lines.Select(x => x.Item2).Order()).Select(a => Math.Abs(a.First - a.Second)).Sum();
+var sumpart2 = lines.Select(x => { group.TryGetValue(x.Item1, out int mult);  return x.Item1 * mult; }).Sum();
+Console.WriteLine("Part 1: " + sumpart1);
+Console.WriteLine("Part 2: " + sumpart2);
---
+++ Day02/Program.cs
@@ -0,0 +1,35 @@
+﻿var lines = File.ReadAllLines("input.txt").Select(x => x.Split(" ").Select(int.Parse)).ToList();
+int sumpart1 = 0;
+int sumpart2 = 0;
+
+foreach (var line in lines)
+{
+    if (is_safe(line))
+    {
+        sumpart1++;
+        sumpart2++;
+        continue;
+    }
+    for (int i = 0; i < line.Count(); i++)
+    {
+        var list = line.ToList();
+        list.RemoveAt(i);
+        if (is_safe(list))
+        {
+            sumpart2++;
+            break;
+        }
+    }
+
+}
+Console.WriteLine("Part 1: " + sumpart1);
+Console.WriteLine("Part 2: " + sumpart2);
+
+static bool is_safe(IEnumerable<int> line)
+{
+    var tuples = line.Zip(line.Skip(1), (a, b) => Tuple.Create(a, b));
+    return tuples.All(pair => Math.Abs(pair.Item1 - pair.Item2) <= 3)
+        && (tuples.All(pair => pair.Item1 < pair.Item2)
+        || tuples.All(pair => pair.Item1 > pair.Item2)
+        );
+}
