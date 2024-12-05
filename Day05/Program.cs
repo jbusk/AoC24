57ce707ad35144e318c165b95fa902e1ed662be1
Johan Busk Eriksson <busk@lysator.liu.se>
2024-12-05 22:36:07 +0100

    Turns out it's faster not using parallel.foreach

--- Day05/Program.cs
+++ Day05/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Concurrent;
-using System.Diagnostics;
-
+﻿using System.Diagnostics;
 var lines = File.ReadAllLines("input.txt");
 var rulelist = lines.TakeWhile(x => !string.IsNullOrWhiteSpace(x)).Select(x => x.Split('|').Select(int.Parse));
 var pagelist = lines.SkipWhile(x => !string.IsNullOrWhiteSpace(x)).Skip(1).Select(x => x.Split(',').Select(int.Parse));
@@ -10,21 +8,17 @@ foreach (var line in rulelist)
     rules.Add((line.First(), line.Last()));
 }
 var comp = new MyComparer(rules);
-(ConcurrentBag<int> part1, ConcurrentBag<int> part2) = ([], []);
+(List<int> part1, List<int> part2) = ([], []);
 Stopwatch sw = Stopwatch.StartNew();
-Parallel.ForEach(pagelist, pages =>
+foreach (var pages in pagelist)
 {
     var sorted = pages.OrderBy(x => x, comp);
     if (pages.SequenceEqual(sorted))
         part1.Add(halfway(sorted));
     else
         part2.Add(halfway(sorted));
-    static int halfway(IEnumerable<int> pages)
-    {
-        return pages.ElementAt(pages.Count() / 2);
-    }
-});
-
+    static int halfway(IEnumerable<int> pages) => pages.ElementAt(pages.Count() / 2);
+}
 sw.Stop();
 Console.WriteLine("Part 1: " + part1.Sum());
 Console.WriteLine("Part 2: " + part2.Sum());
