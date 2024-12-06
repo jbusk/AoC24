59c13013b2a281430c14468442ad9813918c831f
Johan Busk Eriksson <johan.busk.eriksson@liu.se>
2024-12-06 15:20:09 +0100

    day 6: now shorter, better, faster

--- Day06/Program.cs
+++ Day06/Program.cs
@@ -1,125 +1,74 @@
-﻿using System.Collections.Concurrent;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 var lines = File.ReadAllLines("input.txt");
 Dictionary<(int x, int y), char> grid = [];
 for (int x = 0; x < lines.Length; x++)
-{
     for (int y = 0; y < lines[x].Length; y++)
-    {
-        grid[(x,y)] = lines[x][y];
-    }
-}
+        grid[(x, y)] = lines[x][y];
+
 char[] guard = ['^', '<', '>', 'v'];
 var origin = grid.Where(x => guard.Contains(x.Value)).FirstOrDefault();
-(int x, int y)[] neighbours = [(-1, -1), (-1, 1), (1, 1), (1, -1)];
 Stopwatch sw = Stopwatch.StartNew();
 //part 1
-while (true)
-{
-    var curr = grid.Where(x => guard.Contains(x.Value)).FirstOrDefault();
-    if (curr.Value == '\0')
-        break;
-    var cpos = curr.Key;
-    var cval = curr.Value;
-    (int x, int y) npos = curr.Value switch
-    {
-        '^' => ((curr.Key.x - 1), (curr.Key.y)),
-        'v' => ((curr.Key.x + 1), (curr.Key.y)),
-        '<' => ((curr.Key.x), (curr.Key.y - 1)),
-        _ => ((curr.Key.x), (curr.Key.y + 1)), // '>'        
-    };
-    if (grid.TryGetValue(npos, out char nval))
-    {
-        if (nval == '#')
-        {
-            grid[cpos] = curr.Value switch
-            {
-                '^' => '>',
-                '>' => 'v',
-                'v' => '<',
-                _ => '^'
-            };
-        }
-        else
-        {
-            foreach (var neighbour in neighbours)
-            {
-                var neighpos = (cpos.x + neighbour.x, cpos.y + neighbour.y);
-                if (grid.TryGetValue(neighpos, out var neighval))
-                {
-                    if (neighval == '.')
-                        grid[neighpos] = 'O';
-                }
-            }
-            grid[npos] = cval;
-            grid[cpos] = 'X';
-        }
-    }
-    else
-    {
-        grid[cpos] = 'X'; // Elvis has left the building
-    }
-}
+(var cpos, var cval) = (origin.Key, origin.Value);
+GuardLoops(grid, cpos, cval);
 Console.WriteLine("Part 1: " + grid.Where(x => x.Value == 'X').Sum(x => 1));
-Console.WriteLine("Part 1 after " + sw.Elapsed);
+Console.WriteLine("Part 1 time: " + sw.Elapsed);
+sw.Restart();
+
 // part 2
 grid[origin.Key] = origin.Value;
 var possibles = grid.Where(x => x.Value != '.');
-ConcurrentBag<int> part2bag = [];
+int sumpart2 = 0;
 Parallel.ForEach(possibles, pos =>
 {
     var gridcopy = grid.ToDictionary(entry => entry.Key, entry => entry.Value);
-    if (!(pos.Key == origin.Key) && pos.Value != '#')
+    gridcopy[pos.Key] = '#';
+    (var cpos, var cval) = (origin.Key, origin.Value);
+    if (GuardLoops(gridcopy, cpos, cval))
+        Interlocked.Increment(ref sumpart2);
+});
+sw.Stop();
+Console.WriteLine("Part 2: " + sumpart2);
+Console.WriteLine("Part 2 time: " + sw.Elapsed);
+
+static bool GuardLoops(Dictionary<(int x, int y), char> grid, (int x, int y) cpos, char cval)
+{
+    HashSet<((int x, int y), char)> steps = [];
+    while (true)
     {
-        Dictionary<(int x, int y), int> stepgrid = [];
-        HashSet<((int x, int y), char)> steps = [];
-        gridcopy[pos.Key] = '#';
-        while (true)
+        (int x, int y) npos = cval switch
         {
-            var curr = gridcopy.Where(x => guard.Contains(x.Value)).FirstOrDefault();
-            if (curr.Value == '\0')
-                break;
-            (var cpos, var cval) = (curr.Key, curr.Value);
-            (int x, int y) npos = curr.Value switch
+            '^' => ((cpos.x - 1), (cpos.y)),
+            '>' => ((cpos.x), (cpos.y + 1)),
+            'v' => ((cpos.x + 1), (cpos.y)),
+            _ => ((cpos.x), (cpos.y - 1)), // '<'        
+        };
+        if (grid.TryGetValue(npos, out char nval))
+        {
+            if (nval == '#')
             {
-                '^' => ((curr.Key.x - 1), (curr.Key.y)),
-                'v' => ((curr.Key.x + 1), (curr.Key.y)),
-                '<' => ((curr.Key.x), (curr.Key.y - 1)),
-                _ => ((curr.Key.x), (curr.Key.y + 1)), // '>'        
-            };
-            if (gridcopy.TryGetValue(npos, out char nval))
-            {
-                if (nval == '#')
+                cval = cval switch
                 {
-                    gridcopy[cpos] = curr.Value switch
-                    {
-                        '^' => '>',
-                        '>' => 'v',
-                        'v' => '<',
-                        _ => '^' // '<'
-                    };
-                }
-                else
-                {
-                    if (steps.Contains((cpos, cval)))
-                    {
-                        part2bag.Add(1);
-                        break;
-                    }
-                    else
-                        steps.Add((cpos, cval));
-                    gridcopy[npos] = cval;
-                    gridcopy[cpos] = '.';
-                }
+                    '^' => '>',
+                    '>' => 'v',
+                    'v' => '<',
+                    _ => '^' // '<'
+                };
             }
             else
             {
-                break;
+                if (steps.Contains((cpos, cval)))
+                    return true;
+                else
+                    steps.Add((cpos, cval));
+                grid[cpos] = 'X';
+                cpos = npos;
             }
         }
+        else
+        {
+            grid[cpos] = 'X';
+            return false;
+        }
     }
-});
-
-Console.WriteLine("Part 2: " + part2bag.Sum());
-sw.Stop();
-Console.WriteLine("Total time: " + sw.Elapsed);
+}
