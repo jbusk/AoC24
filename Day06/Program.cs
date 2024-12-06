888373f80f879987ad01003de2795e0298c9bfb2
Johan Busk Eriksson <johan.busk.eriksson@liu.se>
2024-12-06 12:24:29 +0100

    Day 6 parts 1 and 2

--- Day06/Program.cs
+++ Day06/Program.cs
@@ -1,53 +1,125 @@
-﻿var lines = File.ReadAllLines("sample.txt");
-(int sumpart1, int sumpart2) = (0, 0);
-//Dictionary<(int x, int y), char> grid = [];
-HashSet<((int x, int y), char)> grid = [];
-
-int x = 0;
-foreach (var line in lines)
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
+var lines = File.ReadAllLines("input.txt");
+Dictionary<(int x, int y), char> grid = [];
+for (int x = 0; x < lines.Length; x++)
 {
-    int y = 0;
-    foreach (var ch in line)
+    for (int y = 0; y < lines[x].Length; y++)
     {
-        grid.Add(((x, y), ch));
-        y++;
+        grid[(x,y)] = lines[x][y];
     }
-    x++;
 }
-var foo = grid.Where(x => x.Item1 == (1000, 1000)).FirstOrDefault().Item2;
-Console.WriteLine( "["+foo +"]" + (foo == '\0'));
 char[] guard = ['^', '<', '>', 'v'];
-
+var origin = grid.Where(x => guard.Contains(x.Value)).FirstOrDefault();
+(int x, int y)[] neighbours = [(-1, -1), (-1, 1), (1, 1), (1, -1)];
+Stopwatch sw = Stopwatch.StartNew();
+//part 1
 while (true)
 {
-    var curr = grid.Where(x => guard.Contains(x.Item2)).FirstOrDefault();
-    if (curr.Item2 == '\0')
+    var curr = grid.Where(x => guard.Contains(x.Value)).FirstOrDefault();
+    if (curr.Value == '\0')
         break;
-    (int x, int y) next = curr.Item2 switch
+    var cpos = curr.Key;
+    var cval = curr.Value;
+    (int x, int y) npos = curr.Value switch
     {
-        '^' => ((curr.Item1.x - 1), (curr.Item1.y)),
-        'v' => ((curr.Item1.x + 1), (curr.Item1.y)),
-        '<' => ((curr.Item1.x), (curr.Item1.y - 1)),
-        _ => ((curr.Item1.x), (curr.Item1.y + 1)), // '>'        
+        '^' => ((curr.Key.x - 1), (curr.Key.y)),
+        'v' => ((curr.Key.x + 1), (curr.Key.y)),
+        '<' => ((curr.Key.x), (curr.Key.y - 1)),
+        _ => ((curr.Key.x), (curr.Key.y + 1)), // '>'        
     };
-    //Console.WriteLine($"Guard is at {curr.Item1.x},{curr.Item1.y} and is pointed {curr.Item2} next is {next.x},{next.y}");
-    var nextpos = grid.Where(x => x.Item1 == next).FirstOrDefault();
-    if (nextpos.Item2 == '#')
-        curr.Item2 = curr.Item2 switch
+    if (grid.TryGetValue(npos, out char nval))
+    {
+        if (nval == '#')
         {
-            '^' => '>',
-            '>' => 'v',
-            'v' => '<',
-            _ => '^'
-        };
-    else if (nextpos.Item2 == '\0')
-        curr.Item2 = 'X';
+            grid[cpos] = curr.Value switch
+            {
+                '^' => '>',
+                '>' => 'v',
+                'v' => '<',
+                _ => '^'
+            };
+        }
+        else
+        {
+            foreach (var neighbour in neighbours)
+            {
+                var neighpos = (cpos.x + neighbour.x, cpos.y + neighbour.y);
+                if (grid.TryGetValue(neighpos, out var neighval))
+                {
+                    if (neighval == '.')
+                        grid[neighpos] = 'O';
+                }
+            }
+            grid[npos] = cval;
+            grid[cpos] = 'X';
+        }
+    }
     else
     {
-        nextpos.Item2 = curr.Item2;
-        curr.Item2 = 'X';
+        grid[cpos] = 'X';
     }
 }
-sumpart1 = grid.Where(x => x.Item2 == 'X').Sum(x => 1);
-Console.WriteLine("Part 1: " + sumpart1);
-Console.WriteLine("Part 2: " + sumpart2);
+Console.WriteLine("Part 1: " + grid.Where(x => x.Value == 'X').Sum(x => 1));
+Console.WriteLine("Part 1 after " + sw.Elapsed);
+// part 2
+grid[origin.Key] = origin.Value;
+var possibles = grid.Where(x => x.Value != '.');
+ConcurrentBag<int> part2bag = new();
+Parallel.ForEach(possibles, pos =>
+{
+    var gridcopy = grid.ToDictionary(entry => entry.Key, entry => entry.Value);
+    if (!(pos.Key == origin.Key) && pos.Value != '#')
+    {
+        Dictionary<(int x, int y), int> stepgrid = [];
+        HashSet<((int x, int y), char)> steps = [];
+        gridcopy[pos.Key] = '#';
+        while (true)
+        {
+            var curr = gridcopy.Where(x => guard.Contains(x.Value)).FirstOrDefault();
+            if (curr.Value == '\0')
+                break;
+            (var currpos, var currval) = (curr.Key, curr.Value);
+            (int x, int y) nextpos = curr.Value switch
+            {
+                '^' => ((curr.Key.x - 1), (curr.Key.y)),
+                'v' => ((curr.Key.x + 1), (curr.Key.y)),
+                '<' => ((curr.Key.x), (curr.Key.y - 1)),
+                _ => ((curr.Key.x), (curr.Key.y + 1)), // '>'        
+            };
+            if (gridcopy.TryGetValue(nextpos, out char nextval))
+            {
+                if (nextval == '#')
+                {
+                    gridcopy[currpos] = curr.Value switch
+                    {
+                        '^' => '>',
+                        '>' => 'v',
+                        'v' => '<',
+                        _ => '^' // '<'
+                    };
+                }
+                else
+                {
+                    if (steps.Contains((currpos, currval)))
+                    {
+                        part2bag.Add(1);
+                        break;
+                    }
+                    else
+                        steps.Add((currpos, currval));
+                    gridcopy[nextpos] = currval;
+                    gridcopy[currpos] = '.';
+                }
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+});
+
+Console.WriteLine("Part 2: " + part2bag.Sum());
+sw.Stop();
+Console.WriteLine("Total time: " + sw.Elapsed);
