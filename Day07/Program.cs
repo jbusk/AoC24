43653206b4d4568c1b8b7d2f8026a97b4abb3943
Johan Busk Eriksson <busk@lysator.liu.se>
2024-12-07 16:23:53 +0100

    Day 7 optimisations

--- Day07/Program.cs
+++ Day07/Program.cs
@@ -1,29 +1,35 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
 var lines = File.ReadAllLines("input.txt").Select(x => x.Split(':'));
 (long sumpart1, long sumpart2) = (0, 0);
 long a(long x, long y) => x + y;
 long m(long x, long y) => x * y;
 long c(long x, long y) => long.Parse($"{x}{y}");
 Stopwatch sw = Stopwatch.StartNew();
-foreach (var line in lines)
+ConcurrentBag<long> p1 = [];
+ConcurrentBag<long> p2 = [];
+Parallel.ForEach(lines, line =>
 {
     var expected = long.Parse(line[0]);
     var numbers = line[1].Trim().Split(' ').Select(long.Parse).Reverse();
     if (Generatesolutions(numbers, [a, m]).Any(x => x == expected))
-        sumpart1 += expected;
-    if (Generatesolutions(numbers, [a, m, c]).Any(x => x == expected))
-        sumpart2 += expected;
-}
+        p1.Add(expected);
+    else if (Generatesolutions(numbers, [a, m, c]).Any(x => x == expected))
+        p2.Add(expected);
+});
+sumpart1 = p1.Sum();
+sumpart2 = sumpart1 + p2.Sum();
 Console.WriteLine("Part 1: " + sumpart1);
 Console.WriteLine("Part 2: " + sumpart2);
 Console.WriteLine("Time:   " + sw.Elapsed);
 static IEnumerable<long> Generatesolutions(IEnumerable<long> numbers, IEnumerable<Func<long, long, long>> funcs)
 {
-    if (!numbers.Any())
-        yield break;
     if (numbers.Count() == 1)
-        yield return numbers.First();
-    foreach (var op in funcs)
+    {
+	yield return numbers.First();
+	yield break;
+    }
+     foreach (var op in funcs)
         foreach (var num in Generatesolutions(numbers.Skip(1), funcs))
             yield return op(num, numbers.First());
 }
