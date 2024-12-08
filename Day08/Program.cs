635a4c19031453e11d631cbb3d9089462be5cd18
Johan Busk Eriksson <busk@lysator.liu.se>
2024-12-08 23:31:07 +0100

    Day 8 final for real

--- Day08/Program.cs
+++ Day08/Program.cs
@@ -2,24 +2,39 @@
 Stopwatch sw = Stopwatch.StartNew();
 var lines = File.ReadAllLines("input.txt");
 (HashSet<(int x, int y)> part1, HashSet<(int x, int y)> part2, int maxpos) = ([], [], lines.Length);
-Dictionary<(int x, int y), char> grid = [];
+HashSet<(char, (int x, int y))> grid = [];
 for (int x = 0; x < lines.Length; x++)
     for (int y = 0; y < lines[x].Length; y++)
-	if (lines[x][y] != '.')
-	    grid[(x, y)] = lines[x][y];
-foreach (var pos in grid)
+        if (lines[x][y] != '.')
+            grid.Add((lines[x][y], (x, y)));
+foreach (var list in grid.ToLookup(k => k.Item1, v => v.Item2).Select(x => getPairs(x)))
 {
-    foreach (var antenna in grid.Where(x => x.Key != pos.Key && x.Value == pos.Value))
+    foreach (var pair in list)
     {
-	if (getAnte(out (int x, int y) ante, pos.Key, antenna.Key, maxpos))
-	    part1.Add(ante);
-        part2.UnionWith(getResonance(pos.Key, antenna.Key, maxpos));
+        if (getAnte(out (int x, int y) ante, pair.Item1, pair.Item2, maxpos))
+            part1.Add(ante);
+        if (getAnte(out ante, pair.Item2, pair.Item1, maxpos))
+            part1.Add(ante);
+        part2.UnionWith(getResonance(pair.Item1, pair.Item2, maxpos));
+        part2.UnionWith(getResonance(pair.Item2, pair.Item1, maxpos));
     }
 }
+
 sw.Stop();
 Console.WriteLine("Part 1: " + part1.Count);
 Console.WriteLine("Part 2: " + part2.Count);
 Console.WriteLine("Total time: " + sw.Elapsed);
+
+static List<((int x, int y), (int x, int y))> getPairs(IEnumerable<(int x, int y)> input)
+{
+    int length = input.Count();
+    List<((int x, int y), (int x, int y))> pairs = [];
+    for (int i = 0; i < length; i++)
+        for (int j = i + 1; j < length; j++)
+            if (j < length)
+                pairs.Add((input.ElementAt(i), input.ElementAt(j)));
+    return pairs;
+}
 
 static bool inRange((int x, int y) pos, int max) => (pos.x < max && pos.y < max && pos.x >= 0 && pos.y >= 0);
 
