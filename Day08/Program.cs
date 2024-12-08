9f4e112aa730d54e30b27613e25631749e929866
Johan Busk Eriksson <busk@lysator.liu.se>
2024-12-08 17:37:45 +0100

    Some optimisation day 8

--- Day08/Program.cs
+++ Day08/Program.cs
@@ -5,20 +5,20 @@ int maxpos = lines.Length;
 Dictionary<(int x, int y), char> grid = [];
 for (int x = 0; x < lines.Length; x++)
     for (int y = 0; y < lines[x].Length; y++)
-        grid[(x, y)] = lines[x][y];
+	if (lines[x][y] != '.')
+	    grid[(x, y)] = lines[x][y];
 (HashSet<(int x, int y)> part1, HashSet<(int x, int y)> part2) = ([], []);
-foreach (var pos in grid.Where(x => x.Value != '.'))
+foreach (var pos in grid)
 {
     var other_antennas = grid.Where(x => x.Key != pos.Key && x.Value == pos.Value);
     foreach (var antenna in other_antennas)
     {
-        var anteposition = getAnteposition(pos.Key, antenna.Key);
-        var resonances = getAllResonancePositions(pos.Key, antenna.Key, maxpos);
-	if (inRange(anteposition, maxpos))
-	    part1.Add(anteposition);
-        foreach (var resonance in resonances)
-            if (grid.ContainsKey(resonance)) 
-		part2.Add(resonance);
+        var ante = getAnte(pos.Key, antenna.Key);
+        var resonances = getResonance(pos.Key, antenna.Key, maxpos);
+	if (inRange(ante, maxpos))
+	    part1.Add(ante);
+        foreach (var node in resonances)
+		part2.Add(node);
     }
 }
 sw.Stop();
@@ -28,10 +28,10 @@ Console.WriteLine("Total time: " + sw.Elapsed);
 
 static bool inRange((int x, int y) pos, int max) => (pos.x < max && pos.y < max && pos.x >= 0 && pos.y >= 0);
 
-static (int x, int y) getAnteposition((int x, int y) me, (int x, int y) them)
+static (int x, int y) getAnte((int x, int y) me, (int x, int y) them)
 {
-    var xdiff = Math.Max(me.x, them.x) - Math.Min(me.x, them.x);
-    var ydiff = Math.Max(me.y, them.y) - Math.Min(me.y, them.y);
+    var xdiff = Math.Abs(me.x - them.x);
+    var ydiff = Math.Abs(me.y - them.y);
     if (them.x > me.x)
         xdiff = -1 * xdiff;
     if (them.y > me.y)
@@ -40,19 +40,19 @@ static (int x, int y) getAnteposition((int x, int y) me, (int x, int y) them)
     return me;
 }
 
-static IEnumerable<(int x, int y)> getAllResonancePositions((int x, int y) me, (int x, int y) them, int max)
+static IEnumerable<(int x, int y)> getResonance((int x, int y) me, (int x, int y) them, int max)
 {
-    List<(int x, int y)> retval = [me];
-    var xdiff = Math.Max(me.x, them.x) - Math.Min(me.x, them.x);
-    var ydiff = Math.Max(me.y, them.y) - Math.Min(me.y, them.y);
+    HashSet<(int x, int y)> retval = [];
+    var xdiff = Math.Abs(me.x - them.x);
+    var ydiff = Math.Abs(me.y - them.y);
     if (them.x > me.x)
         xdiff = -1 * xdiff;
     if (them.y > me.y)
         ydiff = -1 * ydiff;
     while (inRange(me, max))
     {
-        me = (me.x + xdiff, me.y + ydiff);
         retval.Add(me);
+        me = (me.x + xdiff, me.y + ydiff);
     }
     return retval;
 }
