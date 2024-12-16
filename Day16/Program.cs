862992ca006af5ad503f029835be83b4f1f14cf4
Johan Busk Eriksson <johan.busk.eriksson@liu.se>
2024-12-16 11:10:30 +0100

    Day 16 part 1

--- Day16/Program.cs
+++ Day16/Program.cs
@@ -1,6 +1,6 @@
 ﻿using Position = (int x, int y);
-var lines = File.ReadAllLines("sample.txt").ToArray();
-Dictionary<Position, Node> grid = [];
+var lines = File.ReadAllLines("input.txt").ToArray();
+Dictionary<Position, char> grid = [];
 Position end = (0, 0);
 Position start = (0, 0);
 for (int y = 0; y < lines.Length; y++)
@@ -10,77 +10,42 @@ for (int y = 0; y < lines.Length; y++)
             end = (x, y);
         else if (lines[y][x] == 'S')
             start = (x, y);
-        grid[(x, y)] = new Node(lines[y][x]);
+        grid[(x, y)] = lines[y][x];
     }
-
-// start at E in the grid
-Console.WriteLine("Part 1: " + findPath(start, end));
-
-int findPath(Position start, Position end)
+PriorityQueue<(Position, char direction, int cost), int> queue = new();
+queue.Enqueue((start, '>', 0), 0);
+HashSet<(char direction, Position)> visited = [];
+List<int> costs = [];
+while (queue.Count > 0)
 {
-    Direction dir = Direction.E;
-
-    return 0;
-}
-
-IEnumerable<Position> getNeighbours(Position pos)
-{
-    Position[] relpositions = [(0, 1), (0, -1), (1, 0), (-1, 0)];
-    foreach (var rel in relpositions)
+    var (position, direction, cost) = queue.Dequeue();
+    if (visited.Contains((direction, position)))
+        continue;
+    visited.Add((direction, position));
+    if (position == end)
     {
-        yield return (pos.x + rel.x, pos.y + rel.y);
+        costs.Add(cost);
     }
-    yield break;
-}
-
-enum Direction
-{
-    N,
-    E,
-    S,
-    W
-}
-
-class Node
-{
-
-    public Node(char c)
+    foreach (var n in getNeighbours(position, direction))
     {
-        if (c == '.')
-        {
-            Visited = false;
-            Path = true;
-            Start = false;
-            End = false;
-        }
-        else if (c == 'S')
-        {
-            Path = true;
-            Visited = false;
-            Start = true;
-            End = false;
-        }
-        else if (c == 'E')
-        {
-            Path = true;
-            Visited = false;
-            Start = false;
-            End = true;
-        }
-        else
-        {
-            Visited = true;
-            Path = false;
-            Start = false;
-            End = false;
-        }
+        if (grid[n.p] == '#')
+            continue;
+        if (visited.Contains((n.d, n.p)))
+            continue;
+        queue.Enqueue((n.p, n.d, n.cost + cost), n.cost + cost);
     }
-    public bool Start { get; set; }
-    public bool End { get; set; }
-    public bool Visited { get; set; }
-    public bool Path { get; set; }
-    public int CostN { get; set; }
-    public int CostE { get; set; }
-    public int CostS { get; set; }
-    public int CostW { get; set; }
 }
+
+Console.WriteLine("Part 1: " + costs.Min());
+
+static (char d, Position p, int cost)[] getNeighbours(Position pos, char dir)
+{
+    return dir switch
+    {
+        '^' => [('^', (pos.x, pos.y - 1), 1), ('<', (pos.x - 1, pos.y), 1001), ('>', (pos.x + 1, pos.y), 1001)], // North West East
+        '>' => [('>', (pos.x + 1, pos.y), 1), ('^', (pos.x, pos.y - 1), 1001), ('v', (pos.x, pos.y + 1), 1001)], // East North South
+        'v' => [('v', (pos.x, pos.y + 1), 1), ('>', (pos.x + 1, pos.y), 1001), ('<', (pos.x - 1, pos.y), 1001)], // South East West
+        _   => [('<', (pos.x - 1, pos.y), 1), ('v', (pos.x, pos.y + 1), 1001), ('^', (pos.x, pos.y - 1), 1001)] // West South North
+    };
+}
+
