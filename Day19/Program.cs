2f830bfb8a7be9e7e6c19e08f37bf20d348b862c
Johan Busk Eriksson <johan.busk.eriksson@liu.se>
2024-12-19 14:21:10 +0100

    day 19 cleanup

--- Day19/Program.cs
+++ Day19/Program.cs
@@ -1,27 +1,22 @@
-﻿using System.Diagnostics;
-
-(int sumpart1, long sumpart2) = (0, 0);
+﻿(int sumpart1, long sumpart2) = (0, 0);
 var lines = File.ReadAllLines("input.txt");
 var patterns = lines[0].Split(", ").ToHashSet();
 Dictionary<string, long> cache = [];
 int maxlength = patterns.MaxBy(x => x.Length)!.Length;
-Stopwatch sw = Stopwatch.StartNew();
 for (int i = 2; i < lines.Length; i++)
 {
     var line = lines[i];
-    //Console.WriteLine("it is " + line);
     long arr = arrangements(line);
     if (arr > 0)
     {
         sumpart1++;
         sumpart2 += arr;
     }
-    //break;
 }
 
 Console.WriteLine(sumpart1);
 Console.WriteLine(sumpart2);
-Console.WriteLine(sw.Elapsed);
+
 long arrangements(string design)
 {
     if (string.IsNullOrWhiteSpace(design)) return 1;
