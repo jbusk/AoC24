c190942e58b8407b1b239afc595b285e0ad270fb
Johan Busk Eriksson <busk@lysator.liu.se>
2024-12-20 07:05:14 +0100

    20 p2 wip

--- Day20/Program.cs
+++ Day20/Program.cs
@@ -20,7 +20,7 @@ for (int y = 0; y < max; y++)
 int initial_distance = navigate(grid, start, end);
 Console.WriteLine("Initial distance: " + initial_distance);
 int aim_for = initial_distance - 100;
-// 2. for each touched wall, remove it and run the course again, seeing how much time was saved
+// 2. for each wall, remove it and run the course again, seeing how much time was saved
 foreach (Position position in grid)
 {
     var ngrid = grid.ToHashSet();
@@ -30,7 +30,18 @@ foreach (Position position in grid)
         sumpart1++;
 }
 Console.WriteLine("Part 1: " + sumpart1);
+Dictionary<(Position cstart, Position cend), int> cheats = [];
+// 3. for each wall, remove it and run the navigate2 , seeing how much time was saved
 
+for (int i = 0; i < aim_for; i++)
+{
+    var ndist = navigate2(grid, start, end, i, out var cstart, out var cend);
+    cheats[(cstart, cend)] = ndist;
+}
+
+sumpart2 = cheats.Count(x => x.Value <= aim_for);
+
+Console.WriteLine("Part 2: " + sumpart2);
 int navigate(HashSet<Position> grid, Position start, Position end)
 {
     LinkedList<(Position position, int cost)> list = [];
@@ -52,6 +63,52 @@ int navigate(HashSet<Position> grid, Position start, Position end)
     }
     return int.MaxValue;
 }
+
+
+int navigate2(HashSet<Position> grid, Position start, Position end, int start_cheating_at, out Position cstart, out Position cend)
+{
+    cstart = (0, 0);
+    cend = (0, 0);
+    LinkedList<(Position position, int cost)> list = [];
+    HashSet<Position> seen = [];
+    list.AddLast((start, 0));
+    bool can_cheat = false;
+    int cheat_steps = 20;
+    for (var node = list.First; node != null; node = node.Next)
+    {
+        if (start_cheating_at == 0)
+        {
+            can_cheat = true;
+            cstart = node.Value.position;
+        }
+        else
+            start_cheating_at--;
+        if (cheat_steps == 0)
+            can_cheat = false;
+
+        foreach (var n in neighbours(node.Value.position))
+        {
+            if (seen.Contains(n))
+                continue;
+            if (grid.Contains(n) && !can_cheat)
+            {
+                cheat_steps--;
+                continue;
+            }
+            if (!grid.Contains(n) && can_cheat)
+            {
+                can_cheat = false;
+                cend = n;
+            }
+            if (n == end)
+                return node.Value.cost + 1;
+            seen.Add(n);
+            list.AddLast((n, node.Value.cost + 1));
+        }
+    }
+    return int.MaxValue;
+}
+
 
 IEnumerable<Position> neighbours(Position pos)
 {
